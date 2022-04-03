60613f1b943ff68b422ee953cb0a4bfc97762cb7
xiao hui <80144167+mouxiaohui@users.noreply.github.com>
2022-04-03 17:49:16 +0800

    加载动画

--- main.go
+++ main.go
@@ -1,8 +1,31 @@
 package main
 
 import (
+	"log"
+	"time"
+
 	_ "github.com/mouxiaohui/err-nil/cmd"
+
+	"github.com/briandowns/spinner"
 )
 
 func main() {
+	if err := run(); err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
+func run() error {
+	// 加载动画
+	spinner := spinner.New(spinner.CharSets[9], 100*time.Millisecond)
+	spinner.Color("yellow", "bold")
+	spinner.Suffix = " 查找中..., 骚等 🤗"
+	spinner.Start()
+
+	time.Sleep(4 * time.Second) // Run for some time to simulate work
+
+	// 停止加载动画
+	spinner.Stop()
+
+	return nil
 }
